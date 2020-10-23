19f162c322c5968238134b722ec4863bb01a47dc
volpertinger <merzlovnik@mail.ru>
2020-10-23 21:18:02 +0300

    cmake fix

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,8 +1,6 @@
 // Copyright 2020 Merzlov Nikolay merzlovnik@mail.ru
 
-#include <gtest/gtest.h>
 #include <boost/filesystem.hpp>
-
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+int main(){
+  return 0;
 }
