bba52cb4512ad1d71200cdd10fbf778728f496a7
volpertinger <merzlovnik@mail.ru>
2020-10-31 14:00:19 +0300

    final_1

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,10 +3,16 @@
 #include <boost/filesystem.hpp>
 #include <header.hpp>
 
-int main() {
-  std::string s_p(
-      "/home/killoboker/CLionProjects/lab-04-boost-filesystem/misc");
-  Filesystem ss(s_p);
-  ss.print();
+int main(int argc, char *argv[]) {
+  std::string path;
+  if (argc > 2) {
+    std::cerr << "To much arguments" << std::endl;
+    return 1;
+  }
+  if (argc == 2) path = argv[1];
+  if (argc == 1) std::getline(std::cin, path);
+  if (argc == 0) path = "../";
+  Filesystem fs(path);
+  fs.print();
   return 0;
 }
